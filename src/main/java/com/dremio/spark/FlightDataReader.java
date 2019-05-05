ffb02890d3334741a0dc3873dfbe430bca18d7fd
Ryan Murray <rymurr@gmail.com>
2019-05-05 11:51:29 +0100

    moved to spark 2.4, started to add filter

--- src/main/java/com/dremio/spark/FlightDataReader.java
+++ src/main/java/com/dremio/spark/FlightDataReader.java
@@ -1,88 +1,60 @@
 package com.dremio.spark;
 
 import org.apache.arrow.flight.FlightClient;
-import org.apache.arrow.flight.FlightEndpoint;
 import org.apache.arrow.flight.FlightStream;
 import org.apache.arrow.flight.Location;
 import org.apache.arrow.flight.Ticket;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
-import org.apache.arrow.vector.types.pojo.Schema;
-import org.apache.arrow.vector.util.Text;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
-import org.apache.spark.sql.sources.v2.reader.DataReader;
-import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.sources.v2.reader.InputPartitionReader;
+import org.apache.spark.sql.vectorized.ArrowColumnVector;
+import org.apache.spark.sql.vectorized.ColumnVector;
+import org.apache.spark.sql.vectorized.ColumnarBatch;
 
 
 import java.io.IOException;
 
-public class FlightDataReader implements DataReader<Row> {
+public class FlightDataReader implements InputPartitionReader<ColumnarBatch> {
     private final FlightClient client;
     private final FlightStream stream;
-//    private final int schemaSize;
     private final BufferAllocator allocator;
-//    private final Schema schema;
-    private final StructType structType;
-    private int subCount = 0;
-    private int currentRow = 0;
 
     public FlightDataReader(
             byte[] ticket,
-            BufferAllocator allocator,
             String defaultHost,
-            int defaultPort,
-//            Schema schema,
-            StructType structType) {
+            int defaultPort) {
         this.allocator = new RootAllocator();
-//        this.schema = schema;
-        this.structType = structType;
         client = new FlightClient(this.allocator, new Location(defaultHost, defaultPort)); //todo multiple locations
         client.authenticateBasic("dremio", "dremio123");
         stream = client.getStream(new Ticket(ticket));
-//        schemaSize = structType.size();
-
     }
 
     @Override
     public boolean next() throws IOException {
-        if (subCount == currentRow) {
-            boolean hasNext = stream.next();
-            if (!hasNext) {
-                return false;
-            }
-            subCount = stream.getRoot().getRowCount();
-            currentRow = 0;
-        }
-        return true;
+        return stream.next();
     }
 
     @Override
-    public Row get() {
-        Row row = new GenericRowWithSchema(stream.getRoot().getFieldVectors()
-                .stream()
-                .map(v -> {
-                    Object o = v.getObject(currentRow);
-                    if (o instanceof Text) {
-                        return o.toString();
-                    }
-                    return o;
-                })
-                .toArray(Object[]::new),
-                structType);
-        currentRow++;
-        return row;
+    public ColumnarBatch get() {
+        ColumnarBatch batch = new ColumnarBatch(
+                stream.getRoot().getFieldVectors()
+                        .stream()
+                        .map(ArrowColumnVector::new)
+                        .toArray(ColumnVector[]::new)
+        );
+        batch.setNumRows(stream.getRoot().getRowCount());
+        return batch;
     }
 
     @Override
     public void close() throws IOException {
 
-        try {
+//        try {
 //            client.close();
 //            stream.close();
 //            allocator.close();
-        } catch (Exception e) {
-            throw new IOException(e);
-        }
+//        } catch (Exception e) {
+//            throw new IOException(e);
+//        }
     }
 }
--- src/main/java/com/dremio/spark/FlightDataReaderFactory.java
+++ src/main/java/com/dremio/spark/FlightDataReaderFactory.java
@@ -1,38 +1,22 @@
 package com.dremio.spark;
 
-import org.apache.arrow.flight.FlightClient;
-import org.apache.arrow.flight.FlightEndpoint;
-import org.apache.arrow.flight.Location;
-import org.apache.arrow.flight.Ticket;
-import org.apache.arrow.memory.BufferAllocator;
-import org.apache.arrow.vector.types.pojo.Schema;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.sources.v2.reader.DataReader;
-import org.apache.spark.sql.sources.v2.reader.DataReaderFactory;
-import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.sources.v2.reader.InputPartition;
+import org.apache.spark.sql.sources.v2.reader.InputPartitionReader;
+import org.apache.spark.sql.vectorized.ColumnarBatch;
 
-public class FlightDataReaderFactory implements DataReaderFactory<Row> {
+public class FlightDataReaderFactory implements InputPartition<ColumnarBatch> {
 
     private byte[] ticket;
-//    private final BufferAllocator allocator;
     private final String defaultHost;
     private final int defaultPort;
-//    private Schema schema;
-    private StructType structType;
 
     public FlightDataReaderFactory(
             byte[] ticket,
-//            BufferAllocator allocator,
             String defaultHost,
-            int defaultPort,
-//            Schema schema,
-            StructType structType) {
+            int defaultPort) {
         this.ticket = ticket;
-//        this.allocator = allocator;
         this.defaultHost = defaultHost;
         this.defaultPort = defaultPort;
-//        this.schema = schema;
-        this.structType = structType;
     }
 
     @Override
@@ -41,7 +25,8 @@ public class FlightDataReaderFactory implements DataReaderFactory<Row> {
     }
 
     @Override
-    public DataReader<Row> createDataReader() {
-        return new FlightDataReader(ticket, null, defaultHost, defaultPort, structType);
+    public InputPartitionReader<ColumnarBatch> createPartitionReader() {
+        return new FlightDataReader(ticket, defaultHost, defaultPort);
     }
+
 }
