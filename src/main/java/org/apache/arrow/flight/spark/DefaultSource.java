8a2aaca071aa64e5b16a939a8afda556cc60cb4c
Kyle Brooks <Kyle.Brooks@8451.com>
2022-04-13 17:09:24 -0400

    Getting futher in tests.  Need to fix unserializable tasks.

--- src/main/java/org/apache/arrow/flight/spark/DefaultSource.java
+++ src/main/java/org/apache/arrow/flight/spark/DefaultSource.java
@@ -13,7 +13,7 @@ import org.apache.spark.sql.sources.DataSourceRegister;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.util.CaseInsensitiveStringMap;
 
-public class FlightDataSource implements TableProvider, DataSourceRegister {
+public class DefaultSource implements TableProvider, DataSourceRegister {
 
   private FlightTable makeTable(CaseInsensitiveStringMap options) {
     String protocol = options.getOrDefault("protocol", "grpc");
